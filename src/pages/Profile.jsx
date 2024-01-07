318f13fc101f94d878c168cf0c5bbc03a4658e19
matihas <matihasek@gmail.com>
2024-01-07 21:57:34 +0100

    Improve adding profile-phote

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,50 @@
+import { useRef } from "react"
 import Textbox from "../components/basic/Textbox"
+import '../styles/Profile.css'
+import { useState } from "react"
 function Profile() {
+
+    const textboxLabels = ["Name", "Surname", "Callname",]
+    const [photoURL, setPhotoURL] = useState('')
+    const [photoRatioClass, setPhotoRatioClass] = useState('')
+    const onUpload = (event) => {
+        console.log(event.target.files[0]);
+        const [file] = event.target.files
+
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function (e) {
+                const image = new Image()
+                image.src = e.target.result
+                image.onload = () => {
+                    if (image.naturalWidth > image.naturalHeight) {
+                        setPhotoRatioClass("more-width")
+                    }
+                    else if (image.naturalHeight > image.naturalWidth) {
+                        setPhotoRatioClass("more-height")
+                    }
+                }
+            }
+            reader.readAsDataURL(file);
+            setPhotoURL(URL.createObjectURL(file));
+        }
+    }
     return (
         <>
-            <Textbox label="profile" />
+            <div className="container">
+                <div className="profile-photo">
+                    <div className={"photo " + photoRatioClass}>
+                        {photoURL && <img src={photoURL}></img>}
+                    </div>
+                    <label htmlFor="photo-upload">
+                        <i>Change photo</i>
+                    </label>
+                    <input id="photo-upload" type="file" onChange={onUpload} accept="image/png, image/jpeg, image/jpg" />
+                </div>
+                <div className="main-info">
+                    {textboxLabels.map(t => (<Textbox label={t} key={t} />))}
+                </div>
+            </div>
         </>
     )
 }
