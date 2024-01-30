4045fa44060d44ab673932c3f0e23991e5121b8b
matihas <matihasek@gmail.com>
2024-01-30 20:41:40 +0100

    Add upload profile picture

--- src/components/complex/ProfilePhoto.jsx
+++ src/components/complex/ProfilePhoto.jsx
@@ -1,10 +1,13 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import '../../styles/ProfilePhoto.css'
+import useAuth from "../../hooks/useAuth";
+import Urls from "../../Consts/Urls";
 
-function ProfilePhoto({ sendPhotoUrl }) {
+function ProfilePhoto() {
     const [photoURL, setPhotoURL] = useState('')
     const [photoRatioClass, setPhotoRatioClass] = useState('')
-
+    const { auth } = useAuth();
+    const photoInputRef = useRef();
     const onUpload = (event) => {
         console.log(event.target.files[0]);
         const [file] = event.target.files
@@ -25,18 +28,45 @@ function ProfilePhoto({ sendPhotoUrl }) {
             }
             reader.readAsDataURL(file);
             setPhotoURL(URL.createObjectURL(file));
-            sendPhotoUrl(URL.createObjectURL(file))
+            setUploaded(true)
         }
     }
+    const discard = (e) => {
+        setPhotoURL("");
+        setUploaded(false);
+    }
+    const accept = async (e) => {
+        const dataFile = new FormData();
+
+        const options = {
+            method: 'PATCH',
+            headers: {
+                'Authorization': 'Bearer ' + auth.token
+            },
+            body: photoInputRef.current.files[0]
+        }
+        let resData
+
+        await fetch(Urls.Back + '/User/SaveProfilePhoto', options)
+            .then(res => {
+                console.log(res)
+            })
+    }
+    const [uploaded, setUploaded] = useState(false);
+    const optionsButtons = (<div className="chose-buttons">
+        <button className="btn red" onClick={discard}>Discard</button>
+        <button className="btn green" onClick={accept}>Accept</button>
+    </div>)
+
     return (
         <>
             <div className={"photo " + photoRatioClass}>
                 {photoURL && <img src={photoURL}></img>}
             </div>
-            <label htmlFor="photo-upload">
+            {uploaded ? optionsButtons : <label htmlFor="photo-upload">
                 <p className="btn gray">Change photo</p>
-            </label>
-            <input id="photo-upload" type="file" onChange={onUpload} accept="image/png, image/jpeg, image/jpg" />
+            </label>}
+            <input id="photo-upload" ref={photoInputRef} type="file" onChange={onUpload} accept="image/png, image/jpeg, image/jpg" />
         </>
     )
 }
